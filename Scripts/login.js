1133ec6616a56db5ef52e89d693d208b1f929a88
LorenzoHzK <matheuslorenzosiqueira1@gmail.com>
2024-09-10 12:16:57 -0300

    modificando o login
    modificando o login e inserindo os links para o php e banco de dados

--- Scripts/login.js
+++ Scripts/login.js
@@ -1,8 +1,8 @@
+// Script para gerenciar a transição entre login e criação de conta
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
 
 var body = document.querySelector("body");
-
 
 btnSignin.addEventListener("click", function () {
    body.className = "sign-in-js"; 
@@ -10,6 +10,26 @@ btnSignin.addEventListener("click", function () {
 
 btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
-})
+});
 
+// Script para gerenciar o modal de login de administrador
+var modal = document.getElementById("loginAdministradorModal");
+var btn = document.getElementById("loginAdministradorBtn");
+var span = document.getElementsByClassName("fechar")[0];
 
+// Quando o botão de login de administrador for clicado, o modal aparece
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Quando o usuário clicar no "X", o modal fecha
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Se o usuário clicar fora do modal, ele fecha
+window.onclick = function(evento) {
+  if (evento.target == modal) {
+    modal.style.display = "none";
+  }
+}
