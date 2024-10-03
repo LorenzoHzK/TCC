2be817acd0a1f888fa38c96a76819576a63dfaae
LorenzoHzK <matheuslorenzosiqueira1@gmail.com>
2024-10-03 07:44:01 -0300

    adicionando as pags de perfil

---
+++ perfil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil Usúario</title>
+</head>
+<body>
+    <div>
+        <img src="" alt="">
+        <span></span>
+    </div>
+</body>
+</html>
