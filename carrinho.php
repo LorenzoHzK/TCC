bdd37ced587073dfb2204a0688dcf9e95febe5f8
LorenzoHzK <matheuslorenzosiqueira1@gmail.com>
2024-11-11 12:03:30 -0300

    atualização do carrinho e inserção do php

--- carrinho.php
+++ carrinho.php
@@ -1,18 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrinho de Compras</title>
+    <link rel="stylesheet" href="./Estilos/carrinho.css"> 
+</head>
+<body>
+
 <?php
 include('autenticacao.php');
 $email = $_SESSION['email'];
-$con = mysqli_connect('localhost','root', '', 'crypto_galaxy');
-$sql = "select * from carrinho, criptos where carrinho.email = '$email' and carrinho.id_crypto=criptos.id";
+$con = mysqli_connect('localhost', 'root', '', 'crypto_galaxy');
+$sql = "SELECT * FROM carrinho, criptos WHERE carrinho.email = '$email' AND carrinho.id_crypto = criptos.id";
 $exe = mysqli_query($con, $sql);    
 $total = 0;
-while($res = mysqli_fetch_array($exe)){
-	$id_car = $res['id_car'];
-	$nome = $res['nome'];
-	$valor = $res['valor'];
-	$total += $valor;
-	echo "<div>Produto: $nome Preço: $valor <a href='del_carrinho.php?id=$id_car'>Remover</a></div>";
+
+echo "<div class='cart-container'>";
+while ($res = mysqli_fetch_array($exe)) {
+    $id_car = $res['id_car'];
+    $nome = $res['nome'];
+    $valor = $res['valor'];
+    $total += $valor;
+
+    echo "<div class='cart-item'>
+            <span class='cart-item-name'>Produto: $nome</span>
+            <span class='cart-item-price'>Preço: $valor</span>
+            <a class='remove-link' href='del_carrinho.php?id=$id_car'>Remover</a>
+          </div>";
 }
-echo"<div>O valor total é:<b> $total</b></div>";
-echo "<div><a href='ver_cripto.php'>Comprar mais</a></div>";
+echo "<div class='cart-total'>O valor total é: <b>$total</b></div>";
+echo "<a class='continue-shopping' href='ver_cripto.php'>Comprar mais</a>";
+echo "</div>";
+
 $fecha = mysqli_close($con);
-?>	
+?>
+
+</body>
+</html>
--- vantangens.php
+++ vantangens.php
@@ -8,32 +8,28 @@
     <link rel="stylesheet" href="./Estilos/vantagens.css">
     <link rel="shortcut icon" href="../Imagens/logo/favicon.ico" type="image/x-icon">
 </head> 
-<body class="layout-principal">
+<body>
     <!-- Banner -->
-    <section class="banner-principal">
-        <div class="container">
-            <div class="row d-flex">
-                <div class="col-md-5">
-                    <div class="texto-fundo">
-                        <h1>Loja de Criptomoedas Inovadora</h1>
-                        <p>A Crypto Galaxy oferece a melhor plataforma para comprar e vender criptomoedas com facilidade e segurança.</p>
-                        <a href="#vantagens">Explore Agora</a>
-                    </div>
-                </div>
-                <div class="col-md-7">
-                    <figure><img src="./Imagens/pagina vantangens - imagens/image-backgorund.png" alt="Imagem de Fundo" /></figure>
-                </div>
-            </div>
+    <section>
+    <div>
+        <div>
+            <h1>Loja de Criptomoedas Inovadora</h1>
+            <p>A Crypto Galaxy oferece a melhor plataforma para comprar e vender criptomoedas com facilidade e segurança.</p>
+            <a href="#vantagens">Explore Agora</a>
         </div>
-    </section>
+        <div>
+            <img src="./Imagens/pagina vantangens - imagens/image-backgorund.png" alt="Imagem de Fundo" />
+        </div>
+    </div>
+</section>
 
     <!-- Hospedagem -->
-    <div class="hospedagem">
-        <div class="container">
-            <div class="titulo-pagina">
+    <div>
+        <div>
+            <div>
                 <h2>Conheça o mundo Crypto</h2>
             </div>
-            <div class="hospedagem-web">
+            <div>
                 <figure><img src="./Imagens/pagina vantangens - imagens/web.png" alt="Hosting" /></figure>
                 <p>Na Crypto Galaxy, oferecemos uma experiência de compra excepcional com segurança e facilidade.</p>
             </div>
@@ -41,50 +37,50 @@
     </div>
 
     <!-- Serviços -->
-    <div id="servicos" class="servicos">
-        <div class="container">
-            <div class="titulo-pagina">
+    <div id="servicos">
+        <div>
+            <div>
                 <h2 id="vantagens">Por que escolher a Crypto Galaxy?</h2>
                 <p>Descubra os principais benefícios da Crypto Galaxy.</p>
             </div>
-            <div class="row">
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                    <div class="caixa-servicos">
+            <div>
+                <div>
+                    <div>
                         <i><img src="./Imagens/pagina vantangens - imagens/service1.png" alt="Interface Intuitiva" /></i>
                         <h3>Interface Intuitiva e Acessível</h3>
                         <p>Facilidade de navegação para todos os usuários.</p>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                    <div class="caixa-servicos">
+                <div>
+                    <div>
                         <i><img src="./Imagens/pagina vantangens - imagens/service2.png" alt="Atualizações em Tempo Real" /></i>
                         <h3>Atualizações em Tempo Real</h3>
                         <p>Dados de mercado em tempo real para decisões rápidas.</p>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                    <div class="caixa-servicos">
+                <div>
+                    <div>
                         <i><img src="./Imagens/pagina vantangens - imagens/service3.png" alt="Segurança Avançada" /></i>
                         <h3>Segurança Avançada</h3>
                         <p>Proteção de dados com tecnologia de criptografia.</p>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                    <div class="caixa-servicos">
+                <div>
+                    <div>
                         <i><img src="./Imagens/pagina vantangens - imagens/service4.png" alt="Diversidade de Criptomoedas" /></i>
                         <h3>Diversidade de Criptomoedas</h3>
                         <p>Oportunidades de investimento diversificadas.</p>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                    <div class="caixa-servicos">
+                <div>
+                    <div>
                         <i><img src="./Imagens/pagina vantangens - imagens/service5.png" alt="Análises de Mercado" /></i>
                         <h3>Análises de Mercado e Insights</h3>
                         <p>Relatórios detalhados para decisões estratégicas.</p>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                    <div class="caixa-servicos">
+                <div>
+                    <div>
                         <i><img src="./Imagens/pagina vantangens - imagens/service6.png" alt="Suporte ao Cliente" /></i>
                         <h3>Suporte ao Cliente 24/7</h3>
                         <p>Atendimento disponível a qualquer hora.</p>
@@ -95,9 +91,9 @@
     </div>
 
     <!-- Sobre a Crypto Galaxy -->
-    <div id="porque" class="porque">
-        <div class="container">
-            <div class="titulo-pagina">
+    <div id="porque">
+        <div>
+            <div>
                 <h2>Crypto Galaxy</h2>
                 <p>Seu ponto de partida para explorar o universo das criptomoedas com segurança e confiança.</p>
             </div>
